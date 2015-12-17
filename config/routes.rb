81c76e9dfb078702d8bff56add944929e409caf5
Rachel Warbelow <rachelwarbelow@gmail.com>
2015-12-17 09:06:06 -0700

    implement song creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :artists, only: [:index, :new, :create, :show]
+  resources :artists, only: [:index, :new, :create, :show] do 
+    resources :songs, only: [:new, :create]
+  end
+
+  resources :songs, only: [:show]
 end
